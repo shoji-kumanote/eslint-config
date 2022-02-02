5dd6de949cb0c124a9033f82bbf42825ad8d1e7c
shoji-kumanote <h-shoji@kumanote.info>
2022-02-02 15:54:22 +0900

    no-restricted-syntaxのルールを追加

--- index.js
+++ index.js
@@ -110,6 +110,23 @@ module.exports = {
         variables: true,
         enums: true
       }
+    ],
+
+    // eslint-config-airbnb-base の no-restricted-syntax から ForOfStatement だけ除く
+    "no-restricted-syntax": [
+      "error",
+      {
+        selector: "ForInStatement",
+        message: "The `for` ... `in` statement should not be used."
+      },
+      {
+        selector: "LabeledStatement",
+        message: "The labeled statement should not be used."
+      },
+      {
+        selector: "WithStatement",
+        message: "The `with` statement should not be used."
+      }
     ]
   }
 };
